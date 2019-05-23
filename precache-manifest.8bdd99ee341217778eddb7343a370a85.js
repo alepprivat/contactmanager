e02629def3d186517b2f302d98184d23dca22eec
Ale Constela <aleconstela@Ales-MacBook-Pro.local>
2019-05-22 22:17:27 -0300

    Updates

---
+++ precache-manifest.8bdd99ee341217778eddb7343a370a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f417f8fda585fd4d5bf030441af782f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "719fca4e2dc71e1b2e8f",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "93f347645f99c0f410c4",
+    "url": "/contactmanager/static/css/main.14261d01.chunk.css"
+  },
+  {
+    "revision": "719fca4e2dc71e1b2e8f",
+    "url": "/contactmanager/static/js/2.055cbd4e.chunk.js"
+  },
+  {
+    "revision": "93f347645f99c0f410c4",
+    "url": "/contactmanager/static/js/main.631f7fc8.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
